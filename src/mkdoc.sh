054c977c550b626d14de86365c2d9c2705c17ece
Matthias Effler <182258399+MatthiasArno@users.noreply.github.com>
2024-10-14 18:34:36 +0200

    added: more meta
    Signed-off-by: Matthias Effler <182258399+MatthiasArno@users.noreply.github.com>

--- src/mkdoc.sh
+++ src/mkdoc.sh
@@ -1,5 +1,7 @@
 if [ "$(basename "$(pwd)")" = "src" ]; then
-	pandoc --standalone --mathjax README.md  -o ../docs/index.html  --metadata title="Heizungsgesetz und Wärmepumpe im Altbau"
+	pandoc --standalone --mathjax README.md  -o ../docs/index.html \
+	 --metadata title="Heizungsgesetz und Wärmepumpe im Altbau" \
+	 --metadata description="SCOP (JAZ) Schätzung einer Wärmepumpe im Altbau (Fachwerk) mit Option Photovoltaik. Wirtschaftlichkeit und Förderbedingung nach Gebäudeenergiegesetz."
 else
 	echo "Change to src folder and run again!"
 fi
